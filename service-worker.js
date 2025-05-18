1d32af06ca6c991af2f31c604389209d831e1b3b
Rosilaine Silva <95878026+rosipapa@users.noreply.github.com>
2025-05-18 15:56:16 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('quiz-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
